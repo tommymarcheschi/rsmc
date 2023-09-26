791d6ec3735f4ac75ef592efb1ab0d139b1e3d1a
RSMC.tech <erik@rsmc.tech>
2023-09-26 17:00:46 -0500

    landing page format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
   import logo from "$lib/images/RSMC-logo-NEW.svg?raw";
 </script>
-<div class="flex items-center justify-center">
-  <div class="flex w-3/5 h-auto">
-    {@html logo}
-  </div>
+
+
+<div class="flex flex-col items-center justify-center">
+  <div class="items-center justify-center w-3/5">{@html logo}</div>
+  <div><h1 class="font-rocks text-white text-center uppercase text-6xl mt-[-0.5em]">A bitcoin agency.</h1></div>
 </div>
 
 
