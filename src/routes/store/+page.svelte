56c2e4fb6c495ead3cfdba487f397b95db89ac45
RSMC.tech <erik@rsmc.tech>
2023-09-28 13:31:38 -0500

    donate3.5

--- src/routes/store/+page.svelte
+++ src/routes/store/+page.svelte
@@ -4,8 +4,8 @@
 </script>
 
 
-<div class="flex flex-col items-center justify-center w-full h-full ">
-  <div class="flex mt-32 mb-16 "><img src="{logowithtype}" alt="logo with type" class="w-full h-auto mb-4" /></div>
+<div class="flex flex-col items-center justify-center w-full ">
+  <div class="flex mt-32 mb-16 "><img src="{logowithtype}" alt="logo with type" class="w-3/5 mb-4" /></div>
   <div class="flex flex-col">
   	<h1 class="font-rocks text-white text-center uppercase text-6xl">STORE → Coming soon!</h1>
   	<span class=" font-bahiana text-white text-center text-2xl mb-4"> &lt;!-- SITE UNDEr CONStRUCTION --&gt;</span>
