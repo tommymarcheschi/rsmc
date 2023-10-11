e646edf47e5eb73e1949a9872d08e5479cecba7e
RSMC.tech <erik@rsmc.tech>
2023-10-11 15:13:07 -0500

    pwa logo custom

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,17 @@ import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		SvelteKitPWA({/* pwa options */})
-		]
-});
+		SvelteKitPWA({
+			manifest: {
+			icons: [
+			{
+			src: '/favicon.png', // replace this with the path to your icon file
+            sizes: '32x32',
+            type: 'image/png',
+            },
+
+          ],
+        },
+      }),
+    ]
+  });
