c2901b9196cd3f52335994952f8e60442e2da7b7
RSMC.tech <erik@rsmc.tech>
2023-10-11 14:29:20 -0500

    pwa ready

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,34 @@
 <script>
+  import { onMount } from 'svelte'
   import "../app.css";
   import Navbar from "$lib/components/Navbar.svelte";
   import Newsletter from "$lib/components/Newsletter.svelte";
   import Footer from "$lib/components/Footer.svelte";
+  import { pwaInfo } from 'virtual:pwa-info'; 
+	import ReloadPrompt from '$lib/ReloadPrompt.svelte';
 
+  
+  onMount(async () => {
+       if (pwaInfo) {
+         const { registerSW } = await import('virtual:pwa-register')
+         registerSW({
+           immediate: true,
+           onRegistered(r) {
+             console.log(`SW Registered: ${r}`)
+           },
+           onRegisterError(error) {
+             console.log('SW registration error', error)
+           }
+         })
+       }
+     })
+     $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '' 
 </script>
+
+<svelte:head> 
+  {@html webManifestLink} 
+</svelte:head>
+
 
   <div class="bg-black h-screen w-screen h-screen -z-20 fixed"> </div>
 
@@ -18,3 +42,8 @@
   <Newsletter />
   <Footer />
 </div>
+
+
+{#await import('$lib/ReloadPrompt.svelte') then { default: ReloadPrompt}}
+  <ReloadPrompt />
+{/await}
