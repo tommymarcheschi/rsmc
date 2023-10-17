ec36f9044fedb65b75230b48e23bf25c6e8d9eb4
RSMC.tech <erik@rsmc.tech>
2023-10-17 15:06:18 -0500

    fix

--- src/routes/api/sendEmail.ts
+++ src/routes/api/sendEmail.ts
@@ -1,4 +1,4 @@
-import { RequestHandler } from '@sveltejs/kit';
+import type { RequestHandler } from '@sveltejs/kit';
 import mailgun from 'mailgun-js';
 
 const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY as string, domain: process.env.MAILGUN_DOMAIN as string });
