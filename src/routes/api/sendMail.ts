96e058da8469418cf4e96988bc91af7192146ed4
RSMC.tech <erik@rsmc.tech>
2023-10-17 13:56:58 -0500

    mailgun js

---
+++ src/routes/api/sendMail.ts
@@ -0,0 +1,50 @@
+import { RequestHandler } from '@sveltejs/kit';
+import mailgun from 'mailgun-js';
+
+const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY as string, domain: process.env.MAILGUN_DOMAIN as string });
+
+export const post: RequestHandler = async (request) => {
+    const { name, email, message } = request.body as { name: string; email: string; message: string };
+
+    // Basic validation
+    if (!name || name.length < 3) {
+        return {
+            status: 400,
+            body: { error: 'Name is required and should be at least 3 characters long.' }
+        };
+    }
+
+    if (!email || !email.includes('@')) {
+        return {
+            status: 400,
+            body: { error: 'A valid email is required.' }
+        };
+    }
+
+    if (!message || message.length < 10) {
+        return {
+            status: 400,
+            body: { error: 'Message is required and should be at least 10 characters long.' }
+        };
+    }
+
+    const data = {
+        from: email,
+        to: 'info@rsmc.tech',
+        subject: `ContactForm: New message from ${name}`,
+        text: message
+    };
+
+    try {
+        await mg.messages().send(data);
+        return {
+            status: 200,
+            body: { message: 'Email sent successfully' }
+        };
+    } catch (error) {
+        return {
+            status: 500,
+            body: { error: 'Failed to send email' }
+        };
+    }
+}
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -4,14 +4,32 @@
 </script>
 
 
-<div class="flex flex-col items-center justify-center w-full ">
-  <div class="flex mt-32 mb-16 items-center justify-center "><img src="{logowithtype}" alt="logo with type" class="w-3/5 mb-4" /></div>
-  <div class="flex flex-col">
-    <h1 class="font-rocks text-white text-center uppercase text-6xl">A bitcoin agency.</h1>
-    <span class=" font-bahiana text-white text-center text-2xl mb-4"> &lt;!-- SITE UNDEr CONStRUCTION --&gt;</span>
+<div class="flex flex-col items-center justify-center ">
+  <div class="flex flex-col mt-32 text-left w-3/5">  
+    <h1 class="font-rocks text-white text-left uppercase text-6xl mb-2">Contact Us</h1>
+    <span class="font-incon text-white text-left text-sm mb-4">Email us at info@rsmc.tech or message us here:</span>
   </div>
-</div> 
 
 
+    <form action="/api/sendEmail" class="mt-16 mb-10">
+
+      <input type="hidden" name="access_key" value="YOUR_ACCESS_KEY_HERE" /> 
+      <div class="grid gap-6 sm:grid-cols-2">
+        <div class="relative z-0">
+          <input type="text" name="name" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-white focus:border-white focus:outline-none focus:ring-0" placeholder=" " />
+          <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-white peer-focus:dark:text-white">Your name</label>
+        </div>
+        <div class="relative z-0">
+          <input type="text" name="email" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-white focus:border-white focus:outline-none focus:ring-0" placeholder=" " />
+          <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-white peer-focus:dark:text-white">Your email</label>
+        </div>
+        <div class="relative z-0 col-span-2">
+          <textarea name="message" rows="5" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-white focus:border-white focus:outline-none focus:ring-0" placeholder=" "></textarea>
+          <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-white peer-focus:dark:text-white">Your message</label>
+        </div>
+      </div>
+      <button type="submit" class="link mt-5 rounded-none bg-black px-2 py-2 text-white font-incon hover:no-underline">Send message</button>
+    </form>
+  </div>
 
 
