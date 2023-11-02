924f43bd02ad1780c3e021a48e21314fca9edff2
RSMC.tech <erik@rsmc.tech>
2023-11-02 15:50:40 -0500

    safari support meta name apple-mobile-web-app-capable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 
 <div class="flex flex-col items-center justify-center w-full">
-  <div class="flex-initial items-center justify-center mt-12 py-8 md:py-12 lg:py-0 md:mt-4 md:w-2/4 w-5/6">{@html logo}</div>
+  <div class="flex-initial items-center justify-center mt-12 mx-1 py-8 md:py-12 lg:py-0 md:mt-4 md:w-2/4 w-5/6">{@html logo}</div>
   <div><h1 class="font-rocks text-white text-center uppercase text-6xl mt-[-0.4em] mx-4">A bitcoin agency.</h1></div>
 </div>
 
