139193a34729f722a2631bfd16c2be3e5b56e86e
RSMC.tech <erik@rsmc.tech>
2023-11-03 12:05:47 -0500

    redirect permanent

--- src/hooks.ts
+++ src/hooks.ts
@@ -4,6 +4,7 @@ export const handle: Handle = async ({ request, resolve }) => {
   if (request.path === '/Auction') {
     return Response.redirect('/auctions', 301);
   }
+  console.log('Redirecting from /Auction to /auctions');
 
   const response = await resolve(request);
   return response;
