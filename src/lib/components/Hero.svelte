f1362e7586b02c6f23ba098681289cb319d218ee
RSMC.tech <erik@rsmc.tech>
2023-11-09 14:25:53 -0600

    png for mail

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -8,14 +8,18 @@
 
 <a href="/auctions/end-fiat" class="no-underline w-inline-block mt-32">
   <div class="w-[80vw] h-[40vh] md:h-[60vh] flex flex-col justify-end items-start p-2 mb-14 md:mb-20 transition-all duration-300 ease-in bg-hero-pattern bg-[auto,_120%] hover:bg-[auto,_115%] scale-100 hover:p-5 hover:scale-[101%]" style="background-position: 0 0, 20% 30%; background-image: linear-gradient(to right, #000 5%, rgba(219, 219, 219, .14) 55%, rgba(255, 255, 255, 0)), url(/auction/end-fiat/ENDFIAT-inscribe.png)">
-    <div class="absolute left-5 top-5"><span class="loading loading-ring loading-sm md:loading-lg bg-btcorange">  </span></div>
+    
+    <div class="absolute left-5 top-5">
+      <span class="loading loading-ring loading-sm md:loading-lg bg-btcorange">  
+      </span>
+    </div>
 
     <h1 class=" text-5xl text-white font-bold font-rocks mb-4 tracking-wide">Auction Delayed</h1>
     <p class="text-sm md:text-base text-white font-anon ">END FIAT! by Tommy Marcheschi</p>
 
 
     {#if $isAuctionFinished}
-      <p>Auction has finished</p>
+    <h1 class=" text-5xl text-white font-bold font-rocks mb-4 tracking-wide">Auction ended! </h1>
 
     {:else}
       {#if $targetDate !== ''}
