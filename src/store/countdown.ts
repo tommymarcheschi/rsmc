8af9daf189aac34800b6896d2e58f4014043d9a5
kvaga <kvaga@rsmc.tech>
2023-11-11 17:17:26 -0500

    endDate fetch

--- src/store/countdown.ts
+++ src/store/countdown.ts
@@ -1,25 +1,37 @@
-import { writable } from 'svelte/store'
+import { get, writable, derived } from 'svelte/store'
+import { feathersClient } from './feathersClient'
 // import { TARGET_DATE } from '$lib/config'
 
 const startDate = "November 9, 2023 17:20:00 EST"
-const endDate = "November 11, 2023 17:20:00 EST"
+// const endDate = "November 11, 2023 17:20:00 EST"
+
+export const endDate = writable('November 11, 2023 17:20:00 EST')
+const endTimestamp = derived(endDate, ($endDate) => new Date($endDate).getTime());
 
 // !TESTING VALUES:
 // const startDate = "November 9, 2023 13:20:00 EST"
 // const endDate = "November 9, 2023 13:25:00 EST"
 
 const startTimestamp = new Date(startDate).getTime()
-const endTimestamp = new Date(endDate).getTime()
+// const endTimestamp = new Date(endDate).getTime()
 
 export const targetDate = writable('')
 
 export const isPublishActivated = writable(false)
 export const isAuctionFinished = writable(false)
 
+async function getEndDate() {
+  const auctionItem = await feathersClient.service('auction-items').get(1)
+  endDate.set(auctionItem.endDate)
+  console.log(`- new auctionItem.endDate: ${auctionItem.endDate}`)
+}
+getEndDate()
+setInterval(getEndDate, 10000)
+
 let interval: number | undefined
-function startTimer() {
+async function startTimer() {
   const now = Date.now()
-  if (now < endTimestamp) {
+  if (now < get(endTimestamp)) {
     interval = setInterval(checkTime, 1000)
   } else {
     checkTime()
@@ -33,12 +45,12 @@ function checkTime() {
     isAuctionFinished.set(false)
     targetDate.set(startDate)
 
-  } else if (now < endTimestamp) {
+  } else if (now < get(endTimestamp)) {
     isPublishActivated.set(true)
     isAuctionFinished.set(false)
-    targetDate.set(endDate)
+    targetDate.set(get(endDate))
 
-  } else if (now > endTimestamp) {
+  } else if (now > get(endTimestamp)) {
     isPublishActivated.set(false)
     isAuctionFinished.set(true)
     targetDate.set('')
